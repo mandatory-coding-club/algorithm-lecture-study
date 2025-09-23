541e833984c2508d3708af2df345e50c9b7495dd
icw0201 <icw0207@naver.com>
2025-09-24 00:50:45 +0900

    [5]4 문제풀이/chaewon

---
+++ 5_problem-solving-patterns/4_chaewon.js
@@ -0,0 +1,29 @@
+//배열과 int값을 받아서
+//배열의 int값만큼 연속된 요소의 합 중 최대값을 반환하는 함수
+
+function maxSubarraySum(arr, width) {
+    //추가되는 값과 없어지는 값을 비교해서 구한다.
+    
+    //예외 조건 바로 종료
+    if(arr.length < width) return null;
+
+    // 첫 번째 윈도우의 합 계산
+    let maxSum = 0;
+    for(let i = 0; i < width; i++) {
+        maxSum += arr[i];
+    }
+
+    //순회
+    for(let i = width; i < arr.length; i++)
+    {
+        tempSum = maxSum - arr[i - width] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5],2)) // 10
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // 17
+console.log(maxSubarraySum([4,2,1,6],1)) // 6
+console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
+console.log(maxSubarraySum([],4)) // null
