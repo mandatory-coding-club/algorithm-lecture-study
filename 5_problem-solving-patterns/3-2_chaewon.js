22378f26056b0ba7384cb86e965b0529198fce3c
icw0201 <icw0207@naver.com>
2025-09-24 00:40:09 +0900

    [5]3-2 문제풀이/채원 - j는 항상 증가하기에 for문으로 설정하였음

--- 5_problem-solving-patterns/3-2_chaewon.js
+++ 5_problem-solving-patterns/3-2_chaewon.js
@@ -5,15 +5,13 @@ function countUniqueValues(arr){
     //탐색대 j와 j와 이전 숫자가 같은지 체크하는 i를 이용한 이중 포인터 풀이
     let res = 1;
     let i = 0;
-    let j = 1;
 
-    while(j < arr.length)
+    for(let j = 1; j < arr.length; j++)
     {
-        if(arr[i] === arr[j]) j++;
+        if(arr[i] === arr[j]) continue;
         else
         {
             i = j;
-            j++;
             res++;
         }
     }
