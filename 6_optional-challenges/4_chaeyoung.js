fac1742645a16dbf9322ba365ae33f57ca495e46
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-09-23 23:09:52 +0900

    [6]-4 문제풀이 / chaeyoung

---
+++ 6_optional-challenges/4_chaeyoung.js
@@ -0,0 +1,24 @@
+// 4.다중 포인터 - isSubsequence
+// 두 문자열을 받아 첫 번째 문자열의 문자가 두 번째 문자열의 문자의 일부에 포함되는지 확인하는 isSubsequence라는 함수를 작성합니다. 즉, 이 함수는 첫 번째 문자열의 문자가 순서가 바뀌지 않고 두 번째 문자열의 어딘가에 나타나는지 확인해야 합니다.
+
+// 예시:
+
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+// 솔루션에는 최소한 다음과 같은 복잡성이 있어야 합니다:
+
+// Time Complexity - O(N + M)
+
+// Space Complexity - O(1)
+
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  while (i < str1.length && j < str2.length) {
+    if (str1[i] === str2[j]) i++;
+    j++;
+  }
+  return i === str1.length;
+}
