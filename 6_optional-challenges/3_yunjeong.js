6fdb0e5e6067c258f874035720bfe7d7919ac25b
Yunjeong (Jay) So <jaeework@gmail.com>
2025-09-24 12:32:57 +0900

    [6]-3 문제풀이 / yunjeong / 주석 추가

--- 6_optional-challenges/3_yunjeong.js
+++ 6_optional-challenges/3_yunjeong.js
@@ -1,13 +1,19 @@
+// 정렬된 정수 배열과 목표 평균이 주어졌을 때, 배열에 쌍의 평균이 목표 평균과 같은 값의 쌍이 있는지 확인
+// Solution
+// multiple pointers 사용
+// 정렬된 배열이기 때문에, 배열의 양 끝에서부터 시작
+// 1. target 보다 작으면 left++
+// 2. target 보다 크면 right--
 function averagePair(arr, target){
   if (arr.length === 0) { return false; }
   let left = 0;
   let right = arr.length - 1;
   
   while(left < right) {
-    let tempAvg = (arr[left] + arr[right]) / 2.0;
-    if(tempAvg === target) {
+    let avg = (arr[left] + arr[right]) / 2;
+    if(avg === target) {
       return true;
-    } else if (tempAvg > target) {
+    } else if (avg > target) {
       right--;
     } else {
       left++;
