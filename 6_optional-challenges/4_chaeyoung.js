3e89478123d3123d455c3953c819734e67fd279d
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-09-24 12:55:33 +0900

    [6]-4 문제풀이 수정 (조건에 관계없이 증가하는 index for문으로 돌게 수정) / chaeyoung

--- 6_optional-challenges/4_chaeyoung.js
+++ 6_optional-challenges/4_chaeyoung.js
@@ -15,10 +15,13 @@
 
 function isSubsequence(str1, str2) {
   let i = 0;
-  let j = 0;
-  while (i < str1.length && j < str2.length) {
+  for (let j = 0; j < str2.length; j++) {
     if (str1[i] === str2[j]) i++;
-    j++;
   }
   return i === str1.length;
 }
+
+console.log(isSubsequence("hello", "hello world"));
+console.log(isSubsequence("sing", "sting"));
+console.log(isSubsequence("abc", "abracadabra"));
+console.log(isSubsequence("abc", "acb"));
