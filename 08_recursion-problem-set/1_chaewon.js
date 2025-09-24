26120dbd4991c1cd38329304a8b13479ca3bd770
icw0201 <icw0207@naver.com>
2025-09-24 12:05:57 +0900

    [8]-1 문제풀이/chaewon

---
+++ 08_recursion-problem-set/1_chaewon.js
@@ -0,0 +1,10 @@
+// 밑과 지수를 받아 거듭제곱을 재귀함수로 구현.
+
+function power(base, exponent) {
+    if(exponent === 0) return 1;
+    return base * power(base, exponent - 1);
+}
+
+console.log(power(2,0)) // 1
+console.log(power(2,2)) // 4
+console.log(power(2,4)) // 16
