af950f9a6b743a4a999f7f3949a4096a41d6bf2c
Yunjeong (Jay) So <jaeework@gmail.com>
2025-09-24 21:08:41 +0900

    [9]-1 문제풀이 / yunjeong

--- 09_bonus-challenge-recursion/1_yunjeong.js
+++ 09_bonus-challenge-recursion/1_yunjeong.js
@@ -1,4 +1,5 @@
 // 문자열을 받아 그 문자열의 역순인 문자열을 반환하는 재귀함수 작성
+// 마지막 인덱스 글자 + reverse(나머지 배열) 형식으로 구현
 function reverse(str){
   if (str.length === 1) return str;
   return str.slice(-1).concat(reverse(str.slice(0, -1)));
