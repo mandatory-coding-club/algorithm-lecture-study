ab309b72e5e917f83d624e259ec582b7b1d4e3fc
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-09-24 12:36:52 +0900

    [5]-4 문제풀이 수정 tempSum 추가 / chaeyoung

--- 5_problem-solving-patterns/4_chaeyoung.js
+++ 5_problem-solving-patterns/4_chaeyoung.js
@@ -23,16 +23,18 @@
 
 function maxSubarraySum(arr, n) {
   if (n > arr.length) return null;
-  
   let maxSum = 0;
 
   for (let i = 0; i < n; i++) {
     maxSum += arr[i];
   }
 
+  let tempSum = maxSum;
   for (let i = 0; i < arr.length - n; i++) {
-    maxSum = Math.max(maxSum, maxSum - arr[i] + arr[i + n]);
+    tempSum = tempSum - arr[i] + arr[i + n];
+    maxSum = Math.max(maxSum, tempSum);
   }
 
   return maxSum;
 }
+
