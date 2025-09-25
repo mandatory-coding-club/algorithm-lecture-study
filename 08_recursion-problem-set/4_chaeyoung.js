fa574730edd146bb2e8b9c6aea5a312e5aca781d
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-09-25 09:15:48 +0900

    [8]-4 문제풀이/chaeyoung

---
+++ 08_recursion-problem-set/4_chaeyoung.js
@@ -0,0 +1,14 @@
+// 4.recursiveRange
+// 숫자를 받으면 0부터 함수에 전달된 숫자까지의 모든 숫자를 더하는 recursiveRange라는 함수를 작성하시오.
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55
