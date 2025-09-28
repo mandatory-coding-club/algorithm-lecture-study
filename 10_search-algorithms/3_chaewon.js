3d1cc5d46313d5dbb427d3ec305a9dc1b55029c7
icw0201 <icw0207@naver.com>
2025-09-29 03:34:54 +0900

    [10]-3 문제풀이/chaewon/오답 수정

--- 10_search-algorithms/3_chaewon.js
+++ 10_search-algorithms/3_chaewon.js
@@ -3,23 +3,19 @@
 function naiveSearch(long, short)
 {
     let count = 0;
-    let j = 0;
     let i = 0;
     for(let i = 0; i < long.length; i ++)
     {
-        if(long[i] !== short[j]) continue;
-        else
+        for(let j = 0; j < short.length; j++)
         {
-            j++;
-            if(j === short.length)
-            {
-                j = 0;
-                count ++;
-            }
+            if(long[i + j] !== short[j]) break;
+            if(j === short.length - 1) count++;
         }
     }
     return count;
 }
+//오답 : ababa aba 같은 경우를 생각하지 않음. i가 계속 증가하게 했음. 각 글자마다 시작점으로 두고 확인했어야 함!
 
 console.log(naiveSearch("roriefklolsisilol","lol")) // 2
 console.log(naiveSearch("aaa111 aaa 111 aaa","aaa")) // 3
+console.log(naiveSearch("ababab","aba")) // 2
