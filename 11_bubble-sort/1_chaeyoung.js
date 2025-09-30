c03595aa1a8c04d39db33fb01f7cf040081731c7
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-09-30 09:50:17 +0900

    콘솔 제거

--- 11_bubble-sort/1_chaeyoung.js
+++ 11_bubble-sort/1_chaeyoung.js
@@ -16,7 +16,6 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
-      console.log(i, arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
