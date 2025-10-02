133a74d702da77c8ec69f7561cd9aee3d9c36a46
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-10-02 09:59:41 +0900

    [11] 문제풀이 수정(불필요한 루프 범위 수정)/채영

--- 11_bubble-sort/1_chaeyoung.js
+++ 11_bubble-sort/1_chaeyoung.js
@@ -14,7 +14,7 @@
 // - 이미 정렬된 원소는 비교하지 않기 위해 j 루프범위 j < arr.length - i - 1 로 설정
 
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
