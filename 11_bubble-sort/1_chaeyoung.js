f0537a5721be290ad3844b18ecb4e58f3120cb3c
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-10-02 17:05:32 +0900

    [11] 문제풀이 수정(최적화)/채영

--- 11_bubble-sort/1_chaeyoung.js
+++ 11_bubble-sort/1_chaeyoung.js
@@ -15,13 +15,17 @@
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
+    let isSwapped = false;
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        isSwapped = true;
       }
     }
+    if (!isSwapped) break;
+    console.log(i, arr);
   }
   return arr;
 }
 
-console.log(111, bubbleSort([5, 3, 8, 4, 2]));
+console.log(111, bubbleSort([5, 3, 8, 4, 2, 9]));
