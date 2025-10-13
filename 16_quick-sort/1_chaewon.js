eaccd11d157693bec1aedcf05a12beedc4f697f4
icw0201 <icw0207@naver.com>
2025-10-13 16:28:30 +0900

    [16] 문제풀이/chaewon/피드백 수정

--- 16_quick-sort/1_chaewon.js
+++ 16_quick-sort/1_chaewon.js
@@ -4,13 +4,14 @@
 
 //재귀 호출로 최종적으로 완성된 arr 를 반환
 
-function quickSort(arr, pivot = 0)
+function quickSort(arr, start = 0, end = arr.length - 1)
 {
    
-    if(arr.length <= 1) return arr;
+    if(end < start) return;
 
-    let changeIndex = 0; //pivot 값이 최종 자리할 곳
-    for(let i = 0; i < arr.length; i++)
+    let pivot = start;
+    let changeIndex = start; //pivot 값이 최종 자리할 곳
+    for(let i = start; i <= end; i++)
     {
         if(arr[pivot] > arr[i])
         {
@@ -22,7 +23,9 @@ function quickSort(arr, pivot = 0)
 
     [arr[changeIndex], arr[pivot]] = [arr[pivot], arr[changeIndex]]; //pivot 값 최종 위치랑 이동
 
-    return quickSort(arr.slice(0, changeIndex)).concat(arr[changeIndex]).concat(quickSort(arr.slice(changeIndex + 1)));
+    quickSort(arr, start, changeIndex - 1);
+    quickSort(arr, changeIndex + 1, end);
+    return arr;
 }
 
 
