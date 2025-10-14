f0952f22461f09315b0e321aad6451c4996081bd
icw0201 <icw0207@naver.com>
2025-10-14 17:15:19 +0900

    [21] 문제풀이/chaewon

---
+++ 21_stacks-queues/2_chaewon.js
@@ -0,0 +1,61 @@
+//큐 연결 리스트로 구현하기
+//FIFO
+
+class Node 
+{
+    constructor(val)
+    {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue 
+{
+    constructor()
+    {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    enqueue(val)
+    {
+        let newNode = new Node(val);
+        if(!this.first)
+        {
+            this.first = newNode;
+            this.last = newNode;
+        }
+        else
+        {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+    dequeue()
+    {
+        let res = this.first;
+        if(!this.first) return undefined;
+        else if(this.length === 1)
+        {
+            this.first = null;
+            this.last = null;
+        }
+        else
+        {
+            this.first = this.first.next;
+        }
+        res.next = null;
+        this.length--;
+        return res;
+    }
+}
+
+var q = new Queue();
+console.log(q.enqueue("FIRST")); //Queue
+console.log(q.enqueue("SECOND")); //Queue
+console.log(q.enqueue("THIRD")); //Queue
+console.log(q.dequeue()); //FIRST
