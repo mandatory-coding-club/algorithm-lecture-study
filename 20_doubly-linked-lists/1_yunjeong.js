509b6b466ddb6c859839dec6af3c3ed687b2975a
Yunjeong (Jay) So <jaeework@gmail.com>
2025-10-15 13:03:14 +0900

    [20]-1 문제풀이 / yunjeong / 클래스명 오타 수정 및 불필요한 임시 변수 제거

--- 20_doubly-linked-lists/1_yunjeong.js
+++ 20_doubly-linked-lists/1_yunjeong.js
@@ -2,12 +2,11 @@
 // Solution
 // 1. 새로운 노드 생성
 // 2. 기존 리스트가 비어 있으면 head와 tail 모두 newNode
-// 3. 기존 tail을 임시 변수에 저장
-// 4. 임시 변수의 next를 newNode로
-// 5. newNode의 prev를 임시 변수에 저장된 노드로
-// 6. 새로운 tail을 newNode로
-// 7. 리스트의 길이 1만큼 증가
-// 8. 리스트 반환
+// 3. 기존 tail의 next를 newNode로
+// 4. newNode의 prev를 기존 tail로
+// 5. 새로운 tail을 newNode로
+// 6. 리스트의 길이 1만큼 증가
+// 7. 리스트 반환
 class Node {
   constructor(value) {
     this.value = value;
@@ -16,7 +15,7 @@ class Node {
   }
 }
 
-class DoubleyLinkedList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -24,13 +23,15 @@ class DoubleyLinkedList {
   }
   push(value) {
     let newNode = new Node(value);
-    if (!this.head) this.head = newNode;
-    else {
-      let temp = this.tail;
-      temp.next = newNode;
-      newNode.prev = temp;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
     }
-    this.tail = newNode;
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
     this.length++;
     return this;
   }
