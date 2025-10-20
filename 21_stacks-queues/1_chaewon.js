f14d3b782917452bc50e361d0b617e48d5fa5ece
icw0201 <icw0207@naver.com>
2025-10-20 18:39:57 +0900

    [21] 문제풀이/chaewon (#27)
    * [21] 문제풀이/chaewon
    
    * [21] 문제풀이/chaewon/주석 추가
    
    * [21] 문제풀이/chaewon/피드백 수정

---
+++ 21_stacks-queues/1_chaewon.js
@@ -0,0 +1,57 @@
+//스택 연결 리스트로 구현하기
+class Node
+{
+    constructor(val)
+    {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack
+{
+    constructor()
+    {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            newNode.next = this.first;
+            this.first = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+    pop()
+    {
+        if(!this.first) return undefined;
+        let res = this.first;
+        if(this.length === 1)
+        {
+            this.first = null;
+            this.last = null;
+        }
+        else
+        {
+            this.first = res.next;
+        }
+
+        res.next = null;
+        this.length --;
+        return res;
+    }
+}
+
+var stack = new Stack();
+console.log(stack.push("FIRST"));
+console.log(stack.push("SECOND"));
+console.log(stack.pop());
+console.log(stack.pop());
